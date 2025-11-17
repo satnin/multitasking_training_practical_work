d2483fb147bbde40cd1333b7b69240fb3c76a72c
Saturnin Wanonkou <saturninwanonkou17@gmail.com>
2025-11-17 11:49:39 +0100

    frzt : adder

--- exercice-1/messageAdder.c
+++ exercice-1/messageAdder.c
@@ -17,10 +17,15 @@ volatile MSG_BLOCK out;
 //Consumer count storage
 volatile unsigned int consumeCount = 0;
 
+pthread_mutex_t mut_out;
+
 /**
  * Increments the consume count.
  */
-static void incrementConsumeCount(void);
+static void incrementConsumeCount(void)
+{
+	consumeCount++;
+}
 
 /**
  * Consumer entry point.
@@ -30,10 +35,15 @@ static void *sum( void *parameters );
 
 MSG_BLOCK getCurrentSum(){
 	//TODO
+	pthread_mutex_lock(&mut_out);
+	MSG_BLOCK msg = out;
+	pthread_mutex_unlock(&mut_out);
+	return msg;
 }
 
 unsigned int getConsumedCount(){
 	//TODO
+	return consumeCount;
 }
 
 
@@ -44,10 +54,13 @@ void messageAdderInit(void){
 		out.mData[i] = 0;
 	}
 	//TODO
+	pthread_create(&consumer, NULL, sum, NULL);
+	pthread_mutex_init(&mut_out, NULL);
 }
 
 void messageAdderJoin(void){
 	//TODO
+	pthread_join(consumer, NULL);
 }
 
 static void *sum( void *parameters )
@@ -58,9 +71,27 @@ static void *sum( void *parameters )
 		i++;
 		sleep(ADDER_SLEEP_TIME);
 		//TODO
+		MSG_BLOCK new_msg;
+		pthread_mutex_lock(&mut_out);
+		out = getMessage();
+		pthread_mutex_unlock(&mut_out);
+		incrementConsumeCount();
+
+		unsigned int j = 4;
+		while (--j)
+		{
+			new_msg = getMessage();
+			pthread_mutex_lock(&mut_out);
+			messageAdd(&out, &new_msg);
+			pthread_mutex_unlock(&mut_out);
+			incrementConsumeCount();
+			
+		}
+		
 	}
 	printf("[messageAdder] %d termination\n", gettid());
 	//TODO
+	pthread_exit(NULL);
 }
 
 
